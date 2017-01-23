7c8ad003504931d7ad829ae2c90a236d398e9cf6
Tryton Van Meer <trytonvanmeer@gmail.com>
2017-01-23 15:19:28 -0500

    Organized strings.xml and added translations for en-rCA

--- AndroidLabs/app/src/main/java/cst2335/vanm0012/androidlabs/LoginActivity.java
+++ AndroidLabs/app/src/main/java/cst2335/vanm0012/androidlabs/LoginActivity.java
@@ -1,29 +1,91 @@
 package cst2335.vanm0012.androidlabs;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity
+{
+    protected static String ACTIVITY_NAME = "LoginActivity";
+    protected  Button loginButton;
+    protected EditText emailField;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        loginButton = (Button) findViewById(R.id.login_button);
+        emailField = (EditText) findViewById(R.id.email_field);
+
+        loginButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                // Set email in SharedPreferences
+                SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString("DefaultEmail", emailField.getText().toString());
+                editor.apply();
+
+                // Switch to StartActivity
+            }
+        });
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        fab.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view)
+            {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
     }
 
+    protected void onResume()
+    {
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "in onResume()");
+    }
+
+    protected void onStart()
+    {
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "in onStart()");
+
+        // Read from SharedPreferences for saved email and set field
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        emailField.setText(sharedPref.getString("DefaultEmail", "email@domain.com"));
+    }
+
+    protected void onPause()
+    {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "in onPause()");
+    }
+
+    protected void onStop()
+    {
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "in onStop()");
+    }
+
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "in onDestroy()");
+    }
 }
--- AndroidLabs/app/src/main/java/cst2335/vanm0012/androidlabs/StartActivity.java
+++ AndroidLabs/app/src/main/java/cst2335/vanm0012/androidlabs/StartActivity.java
@@ -2,12 +2,46 @@ package cst2335.vanm0012.androidlabs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class StartActivity extends AppCompatActivity {
+public class StartActivity extends AppCompatActivity
+{
+    protected static final String ACTIVITY_NAME = "StartActivity";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+    }
+
+    protected void onResume()
+    {
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "in onResume()");
+    }
+
+    protected void onStart()
+    {
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "in onStart()");
+    }
+
+    protected void onPause()
+    {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "in onPause()");
+    }
+
+    protected void onStop()
+    {
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "in onStop()");
+    }
+
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "in onDestroy()");
     }
 }
