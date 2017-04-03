300515bd5ca13f2d1481783fddd67b4ad5da0466
Tryton Van Meer <trytonvanmeer@gmail.com>
2017-04-03 13:33:43 -0400

    Added methods onCreateOptionsMenu and onOptionsItemSelected

--- AndroidLabs/app/src/main/java/cst2335/vanm0012/androidlabs/TestToolbar.java
+++ AndroidLabs/app/src/main/java/cst2335/vanm0012/androidlabs/TestToolbar.java
@@ -5,25 +5,51 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
-public class TestToolbar extends AppCompatActivity {
+public class TestToolbar extends AppCompatActivity
+{
+    final static String ACTIVITY_NAME = "TestToolbar";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_toolbar);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    public boolean onCreateOptionsMenu(Menu m)
+    {
+        getMenuInflater().inflate(R.menu.toolbar_menu, m);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem mi)
+    {
+        boolean action_performed = false;
+
+        switch (mi.getItemId())
+        {
+            case R.id.action_one:
+                Log.d(ACTIVITY_NAME, "Starting action_one");
+                action_performed = true;
+                break;
+            case R.id.action_two:
+                Log.d(ACTIVITY_NAME, "Starting action_two");
+                action_performed = true;
+                break;
+            case R.id.action_three:
+                Log.d(ACTIVITY_NAME, "Starting action_three");
+                action_performed = true;
+                break;
+        }
+
+        return action_performed;
     }
 
 }
